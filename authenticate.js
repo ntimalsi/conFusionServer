915deace414bd0c727af42f503dc47b231d81c67
Nischal <nischal.timalsina@gmail.com>
2020-12-16 10:44:50 -0600

    HTTPS added

--- authenticate.js
+++ authenticate.js
@@ -36,4 +36,18 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         });
     }));
 
+
+
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+
+exports.verifyAdmin= (req, res, next) =>{
+    if (req.user.admin){
+        next();
+        return;
+    }
+        var err = new Error("You are not authorized to perform this operation");
+        err.status = 403;
+        next(err);
+    
+}
