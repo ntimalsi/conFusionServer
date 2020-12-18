ad37fb790a8c2ecc8e7ed82b46e31692c62613b4
Nischal <nischal.timalsina@gmail.com>
2020-12-18 12:01:17 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '213824320365474',
+        clientSecret: 'b92933a9e80636d882f81b6f710e0e65'
+    }
 }
